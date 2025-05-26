c5439520ce1e82d7aa9f9e7048606c0f3e5d6922
govSite <randomnessomgomg@gmail.com>
2025-05-26 19:25:11 +0530

    added resolve

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +13,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        // Ensure 404.html is generated
+        notFound: resolve(__dirname, "index.html"),
+      },
+    },
 });
